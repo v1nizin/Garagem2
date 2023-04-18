b629fe405964d19801e107fe06c0ccafbf021203
v1nizin <viniwick@outlook.com>
2023-04-18 16:30:47 -0300

    Criação do modelo Cor

--- Garagem2/models.py
+++ Garagem2/models.py
@@ -31,15 +31,3 @@ class Cor(models.Model):
     class Meta:
         verbose_name_plural = "Cores"
 
-class Veiculo(models.Model):
-    preco = models.DecimalField(max_digits=10, decimal_places=2, default=0, null=True, blank=True)
-    ano = models.IntegerField(null=True, blank=True)
-    marca = models.ForeignKey(Marca, on_delete=models.PROTECT, related_name="veiculos")
-    cor = models.ForeignKey(Cor, on_delete=models.PROTECT, related_name="veiculos")
-    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name="veiculos")
-
-    def __str__(self):
-        return f"{self.marca} {self.categoria} {self.cor} {self.ano}"
-    
-    class Meta:
-        verbose_name = "Veículo"
