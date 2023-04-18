14a030fa6065225cd23de02b8b8c5c50493552c9
v1nizin <viniwick@outlook.com>
2023-04-18 16:20:44 -0300

    Criação do modelo Acessorio

--- Garagem2/admin.py
+++ Garagem2/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from Garagem2.models import Categoria, Marca 
+from Garagem2.models import Acessorio,Categoria, Marca 
 
+admin.site.register(Acessorio)
 admin.site.register(Categoria)
 admin.site.register(Marca)
