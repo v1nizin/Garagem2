cd982b6de3a9fd3ad16a491f0f2de50d627285f3
v1nizin <viniwick@outlook.com>
2023-05-02 15:48:53 -0300

    Criação do View Marca

---
+++ Garagem2/views.py
@@ -0,0 +1,8 @@
+from rest_framework.viewsets import ModelViewSet
+
+from Garagem2.models import Marca
+from Garagem2.serializers import MarcaSerializer
+
+class MarcaViewSet(ModelViewSet):
+    queryset = Marca.objects.all()
+    serializer_class = MarcaSerializer
