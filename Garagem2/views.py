61db3010f1a2ca959f26cfc7b259bd77eea858c5
v1nizin <viniwick@outlook.com>
2023-05-02 16:08:58 -0300

    Criação da class no view CategoriaSerializer

--- Garagem2/views.py
+++ Garagem2/views.py
@@ -1,8 +1,13 @@
 from rest_framework.viewsets import ModelViewSet
 
-from Garagem2.models import Marca
-from Garagem2.serializers import MarcaSerializer
+from Garagem2.models import Categoria, Marca
+from Garagem2.serializers import CategoriaSerializer, MarcaSerializer
 
 class MarcaViewSet(ModelViewSet):
     queryset = Marca.objects.all()
     serializer_class = MarcaSerializer
+
+class CategoriaSerializer(ModelViewSet):
+    class Meta:
+        model = Categoria
+        fields = "__all__"
