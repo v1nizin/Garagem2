76d2b5dd93c57698953e719194db198c23eb05c8
v1nizin <viniwick@outlook.com>
2023-05-02 16:05:47 -0300

    Criação da class CategoriaSerializer

--- Garagem2/serializers.py
+++ Garagem2/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework.serializers import ModelSerializer
 
-from Garagem2.models import Marca
+from Garagem2.models import Categoria, Marca
 
 class MarcaSerializer(ModelSerializer):
     class Meta:
         model = Marca
         fields = "__all__"
+
+class CategoriaSerializer(ModelSerializer):
+    class Meta:
+        model = Categoria
+        fields = "__all__"
+
