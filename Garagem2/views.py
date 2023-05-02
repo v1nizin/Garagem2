9d5cc998560ab7e22c0f05f38ae60dca6c8396eb
v1nizin <viniwick@outlook.com>
2023-05-02 15:44:57 -0300

    Criação do serializer

--- Garagem2/views.py
+++ Garagem2/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from Garagem2.models import Marca
+from Garagem2.serializers import MarcaSerializer
+
+class MarcaViewSet(ModelViewSet):
+    queryset = Marca.objects.all()
+    serializer_class = MarcaSerializer
--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Garagem2',
+    'rest_framework',
+
 ]
 
 MIDDLEWARE = [
