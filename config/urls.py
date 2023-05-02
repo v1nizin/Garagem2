3f606aba0cd739976567a61463cbb832124f8e81
v1nizin <viniwick@outlook.com>
2023-05-02 16:14:21 -0300

    Criação do rounter no url Categoria

--- config/urls.py
+++ config/urls.py
@@ -3,9 +3,10 @@ from django.urls import include, path
 
 from rest_framework.routers import DefaultRouter
 
-from Garagem2.views import MarcaViewSet
+from Garagem2.views import CategoriaViewSet, MarcaViewSet
 
 router = DefaultRouter()
+router.register(r"Categorias", CategoriaViewSet)
 router.register(r"Marcas", MarcaViewSet)
 
 urlpatterns = [
