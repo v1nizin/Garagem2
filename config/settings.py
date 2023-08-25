df25c5d65fe5b979f83cb2392b46cb4ee141f859
v1nizin <viniwick@outlook.com>
2023-08-25 10:58:50 -0300

    adicao de imagem de um banco de dados e carros

--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "Garagem2",
     "usuario",
+    "django_extensions",
     "drf_spectacular",
     "uploader",
     "rest_framework",
@@ -137,7 +138,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 FILE_UPLOAD_PERMISSIONS = 0o640
 
 REST_FRAMEWORK = {
-    
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 SPECTACULAR_SETTINGS = {
