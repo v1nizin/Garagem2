25e353f04c5c72b1031ac433175967e67b9d4a59
v1nizin <viniwick@outlook.com>
2023-08-25 10:46:24 -0300

    modificacao do usuario padrao

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 from django.urls import include, path
+from usuario.router import router as usuario_router
 
 from rest_framework.routers import DefaultRouter
 
-from Garagem2.views import MarcaViewSet, CategoriaViewSet, CorViewSet, AcessorioViewSet, ModeloViewSet, VeiculoViewSet
+from Garagem2.views import (
+    MarcaViewSet,
+    CategoriaViewSet,
+    CorViewSet,
+    AcessorioViewSet,
+    ModeloViewSet,
+    VeiculoViewSet,
+)
 
 router = DefaultRouter()
 router.register(r"marcas", MarcaViewSet)
@@ -14,6 +22,7 @@ router.register(r"modelos", ModeloViewSet)
 router.register(r"veiculos", VeiculoViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
     path("", include(router.urls)),
+    path("api/", include(usuario_router.urls)),
 ]
